7d95d4461cb9021920112e2204f87f4e2f338c0c
IsaiahTensonASC4 <isaiah.tenson@gmail.com>
2017-07-18 08:27:08 -0700

    Adding Modifications to sketch

--- p5Projects/drawLoop/sketch.js
+++ p5Projects/drawLoop/sketch.js
@@ -8,20 +8,30 @@ function setup(){
 //}
 
 var x = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+var sentence = ("How you doing Dude")
 var typed = ("")
-
+var Score = 0
 
 function draw(){
     background(0,255,239);
     fill(233, 125, 12);
     textSize(16);
-    text("Please Type the letters that you see.", 125, 30);
+    text(sentence, 155, 120);
+    textSize(20)
+    text("Score:", 40, 250)
+    textSize(20)
+    text(Score, 105, 250)
+    textSize(16)
+    text("Write Here:", 50, 190)
 }
 
 function keyTyped() {
   console.log(typed);
   typed = typed + key;
-
-
+  if(typed == sentence){
+    Score = Score + 1
+  }
 }
+
+
+
